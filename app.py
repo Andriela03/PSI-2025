06827d8a8bc2612fdf655b01211e73b84ddced44
Andriela Gleice Pereira da Silva <20231041110026@ifrn.local>
2025-09-15 14:18:07 -0300

    Aula de Flask - Criando um ambiente Virtual

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Teste'
+
+@app.route('/contato')
+def contato():
+    return 'contato@gmail.com'
+
+if __name__ == "__main__":
+    app.run()
