7c7ae2409d4804ee9d2f4776dc153b77322e90c7
Andriela Gleice Pereira da Silva <20231041110026@ifrn.local>
2025-09-22 14:28:53 -0300

    Aula de flask - passagem parâmetros com Flask

--- app.py
+++ app.py
@@ -8,7 +8,25 @@ def index():
 
 @app.route('/contato')
 def contato():
-    return 'contato@gmail.com'
+    nome = 'maria'
+    email = "maria@gmail.com"
+    return render_template('contato.html', nome = nome, email = email)
+
+@app.route('/perfil', defaults = {'nome': 'fulano'})
+@app.route('/perfil/<usuario>')
+def perfil(usuario):
+    usuario = "fulano"
+    return render_template('perfil.html', usuario = usuario)
+
+
+@app.route('/semestre/<int:x>')
+def semestre(x):
+    y = x + 1
+    return render_template('semestre.html', x=x, y=y)
+
+@app.route('/soma/<int:n1>/<int:n2>')
+def soma (n1, n2):
+    return str(n1+n2)
 
 @app.route('/exemplo')
 def exemplo():
